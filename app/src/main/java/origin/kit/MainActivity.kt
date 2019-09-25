082149e0035b61cb9bf1a2744afd30b10392b030
linjiang <linjiang.me@gmail.com>
2019-09-25 15:22:16 +0800

    test: jitpack

--- app/src/main/java/origin/kit/MainActivity.kt
+++ app/src/main/java/origin/kit/MainActivity.kt
@@ -1,7 +1,8 @@
 package origin.kit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import origin.kit.android.R
 
 class MainActivity : AppCompatActivity() {
 
